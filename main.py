76341d64fbd840110a27545eb09ca68ed520719f
Нурбол Сембаев <231199@astanait.edu.kz>
2024-03-01 17:57:25 +0600

    jlj

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ for i in range(20):
                 textures = "grass.png", parent=scene, origin_y = 0.5)
         boxes.append(box)
 
+def input(key):
+    for box in boxes:
+        if box.hovered :
+            if key == 'right mouse down':
+                new = Button(color=color.white, model=' cube', position=box.position + mouse.normal,
+                            texture = 'grass.png', parent = scene, origin_y = 0.5)
+                boxes.append(new)
+            if key == 'left mouse down':
+                boxes.remove(box)
+                destroy(box)
 app.run()
 
