6a3c3574f0e05eb8a317fcf306cd15451bb0e079
Нурбол Сембаев <231199@astanait.edu.kz>
2024-03-01 18:24:05 +0600

    jlj

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ app = Ursina()
 player = FirstPersonController()
 Sky()
 
-boxes = []
+Blocks = []
 for i in range(20):
     for j in range(20):
-        box = Button(color=color.white, model = "cube", position=(j, 0, i),
-                textures = "grass.png", parent=scene, origin_y = 0.5)
-        boxes.append(box)
-
+        block = Button(color=color.green, model = "cube", position=(j, 0, i),
+                texture = "grass.png", parent=scene, origin_y = 0.5)
+        Blocks.append(block)
+class Block(Button):
+       def __init__(self, position = (0,0,0)):
+            super().__init__(
+                parent = scene,
+                position = position,
+                model = 'cube',
+                texture = 'grass.png',
+                color = color.white,
+                highlight_color = color.black)
+for z in range(8):
+    for x in range(8):
+     Block(position = (x,0,z))
 def input(key):
-    for box in boxes:
-        if box.hovered :
+    for block in blocks:
+        if block.hovered:
             if key == 'right mouse down':
-                new = Button(color=color.white, model= 'sphere', position=box.position + mouse.normal,
+                new = Button(color=color.green, model= 'cube', position=box.position + mouse.normal,
                             texture = 'grass.png', parent = scene, origin_y = 0.5)
                 boxes.append(new)
             if key == 'left mouse down':
-                boxes.remove(box)
-                destroy(box)
+                blocks.remove(block)
+                destroy(block)
 app.run()
 
